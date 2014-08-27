f2ca17714606133d7aab9365dcebf24d63fc5c28
ferrybig <ferrytinymailbox@gmail.com>
2014-08-27 20:22:05 +0200

    added getBlockIdAt(int, int, int) and getBlockDataAt(int, int, int) to EnderWorld

--- src/org/enderstone/server/regions/EnderWorld.java
+++ src/org/enderstone/server/regions/EnderWorld.java
@@ -54,6 +54,16 @@ public class EnderWorld {
 		}
 		return r;
 	}
+	
+	public BlockId getBlockIdAt(int x, int y, int z)
+	{
+	return getOrCreateChunk(x >> 4, z >> 4).getBlock(x & 0xF, y & 0xFF, z & 0xF);
+	}
+	
+	public byte getBlockDataAt(int x, int y, int z)
+	{
+		return getOrCreateChunk(x >> 4, z >> 4).getData(x & 0xF, y & 0xFF, z & 0xF);
+	}
 
 	public synchronized void doChunkUpdatesForPlayer(EnderPlayer player, ChunkInformer informer, int radius) throws Exception {
 
