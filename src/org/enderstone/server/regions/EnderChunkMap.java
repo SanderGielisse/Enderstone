482e88eda047eb72ca11c25d232b0cf92455663b
ferrybig <ferrytinymailbox@gmail.com>
2014-08-28 19:52:01 +0200

    Removed Netbeans comment file headers

--- src/org/enderstone/server/regions/EnderChunkMap.java
+++ src/org/enderstone/server/regions/EnderChunkMap.java
@@ -3,11 +3,6 @@ package org.enderstone.server.regions;
 import java.util.zip.Deflater;
 import org.enderstone.server.packet.play.PacketOutChunkData;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 /**
  *
  * @author Fernando
