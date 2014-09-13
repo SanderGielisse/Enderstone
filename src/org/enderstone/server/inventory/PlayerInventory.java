7b2ac10f15603773b5200160a93ea04940f38d35
ferrybig <ferrytinymailbox@gmail.com>
2014-09-13 18:17:51 +0200

    Added crafting support for player inventory

--- src/org/enderstone/server/inventory/PlayerInventory.java
+++ src/org/enderstone/server/inventory/PlayerInventory.java
@@ -15,7 +15,6 @@
  *     You should have received a copy of the GNU General Public License
  *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
 package org.enderstone.server.inventory;
 
 import java.util.List;
@@ -32,96 +31,93 @@ public class PlayerInventory extends DefaultInventory {
 	private final EnderPlayer player;
 
 	public PlayerInventory(EnderPlayer player) {
-		super(InventoryType.PLAYER_INVENTORY, 45);
+		super(InventoryType.PLAYER_INVENTORY, 45,
+				HalfInventoryListeners.toInventoryListener(
+						new CraftingInventoryListener(2, 2, 0, 1, DefaultCraftingRecepies.recepies)));
 		this.player = player;
 	}
-	
+
 	private transient List<ItemStack> hotbar;
-	public List<ItemStack> getHotbar()
-	{
-		if(hotbar != null) return hotbar;
+
+	public List<ItemStack> getHotbar() {
+		if (hotbar != null) return hotbar;
 		return hotbar = this.items.subList(36, 45);
 	}
-	
+
 	private transient List<ItemStack> topInventory;
-	public List<ItemStack> getTopInventory()
-	{
-		if(topInventory != null) return topInventory;
+
+	public List<ItemStack> getTopInventory() {
+		if (topInventory != null) return topInventory;
 		return topInventory = this.items.subList(9, 36);
 	}
-	
+
 	private transient List<ItemStack> fullInventory;
-	public List<ItemStack> getFullInventory()
-	{
-		if(fullInventory != null) return fullInventory;
+
+	public List<ItemStack> getFullInventory() {
+		if (fullInventory != null) return fullInventory;
 		return fullInventory = this.items.subList(9, 45);
 	}
-	
+
 	private transient List<ItemStack> armor;
-	public List<ItemStack> getArmor()
-	{
-		if(armor != null) return armor;
+
+	public List<ItemStack> getArmor() {
+		if (armor != null) return armor;
 		return armor = this.items.subList(5, 9);
 	}
-	
+
 	private transient List<ItemStack> craftingInputs;
-	public List<ItemStack> getCraftingInputs()
-	{
-		if(craftingInputs != null) return craftingInputs;
+
+	public List<ItemStack> getCraftingInputs() {
+		if (craftingInputs != null) return craftingInputs;
 		return craftingInputs = this.items.subList(1, 5);
 	}
-	
+
 	private transient List<ItemStack> craftingOutputs;
-	public List<ItemStack> getCraftingOutputs()
-	{
-		if(craftingOutputs != null) return craftingOutputs;
+
+	public List<ItemStack> getCraftingOutputs() {
+		if (craftingOutputs != null) return craftingOutputs;
 		return craftingOutputs = this.items.subList(0, 1);
 	}
-	
+
 	private transient List<ItemStack> itemInsertionOrder;
-	public List<ItemStack> getItemInsertionOrder()
-	{
-		if(itemInsertionOrder != null) return itemInsertionOrder;
+
+	public List<ItemStack> getItemInsertionOrder() {
+		if (itemInsertionOrder != null) return itemInsertionOrder;
 		return itemInsertionOrder = new MergedList.Builder<ItemStack>().addList(0, getHotbar(), 0, 9).addList(9, getTopInventory(), 9, 3 * 9).build();
 	}
-	
+
 	@Override
 	public void close0() {
-		this.player.networkManager.disconnect(new SimpleMessage("Main inventory closed (this may not happen!)"),true);
+		this.player.networkManager.disconnect(new SimpleMessage("Main inventory closed (this may not happen!)"), true);
 	}
-	
-	public boolean shiftClickHotbar(int index)
-	{
+
+	public boolean shiftClickHotbar(int index) {
 		// TODO add special handling for armor
 		ItemStack stack = this.getHotbar().get(index);
-		if(stack == null)return false;
+		if (stack == null) return false;
 		int amount = stack.getAmount();
 		ItemStack result = DefaultInventory.tryAddItem(this.getTopInventory(), stack);
-		if(result != null && result.getAmount() == amount)
-		{
+		if (result != null && result.getAmount() == amount) {
 			return true;
 		}
 		this.getHotbar().set(index, result);
 		return true;
 	}
-	
-	public boolean shiftMainInventory(int index)
-	{
+
+	public boolean shiftMainInventory(int index) {
 		// TODO add special handling for armor
 		ItemStack stack = this.getTopInventory().get(index);
-		if(stack == null)return false;
+		if (stack == null) return false;
 		int amount = stack.getAmount();
 		ItemStack result = DefaultInventory.tryAddItem(this.getHotbar(), stack);
-		if(result != null && result.getAmount() == amount)
-		{
+		if (result != null && result.getAmount() == amount) {
 			return true;
 		}
 		this.getTopInventory().set(index, result);
 		return true;
 	}
-	
-	public ItemStack pickUpItem(ItemStack item)
-	{
+
+	public ItemStack pickUpItem(ItemStack item) {
 		return DefaultInventory.tryAddItem(this.getItemInsertionOrder(), item);
 	}
 }
