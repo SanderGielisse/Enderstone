4ada346ccf26840273fcc1c0277be477e0f862ae
BigTeddy98 <sandergielisse@ziggo.nl>
2014-09-16 21:56:09 +0200

    Fixed merge conflict

--- src/org/enderstone/server/packet/NetworkManager.java
+++ src/org/enderstone/server/packet/NetworkManager.java
@@ -264,8 +264,8 @@ public class NetworkManager extends ChannelHandlerAdapter {
 						sendPacket(new PacketOutUpdateHealth(player.getHealth(), player.clientSettings.food, player.clientSettings.foodSaturation));
 						
 						//TODO make this editable with the API
-						sendPacket(new PacketOutTitle(new ActionDisplayTitle(new AdvancedMessage("Welcome to Enderstone").setColor(ChatColor.GOLD).build())));
-						sendPacket(new PacketOutTitle(new ActionSubtitle(new AdvancedMessage("This is an Enderstone test server").setColor(ChatColor.RED).build())));
+						sendPacket(new PacketOutTitle(new ActionDisplayTitle(new AdvancedMessage("Welcome to Enderstone").getBase().setColor(ChatColor.GOLD).build())));
+						sendPacket(new PacketOutTitle(new ActionSubtitle(new AdvancedMessage("This is an Enderstone test server").getBase().setColor(ChatColor.RED).build())));
 					} catch (Exception e) {
 						EnderLogger.exception(e);
 					} finally {
