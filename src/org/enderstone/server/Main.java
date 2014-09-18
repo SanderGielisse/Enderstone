ecb62720b57d206e3d4ce2b99aaa88c2d7883198
ferrybig <ferrytinymailbox@gmail.com>
2014-09-18 14:31:38 +0200

    Fixed wrong polarity on "Can't keep up! xxx behind!", they now show as 124 instead of -124

--- src/org/enderstone/server/Main.java
+++ src/org/enderstone/server/Main.java
@@ -251,7 +251,7 @@ public class Main implements Runnable {
 				this.lastTick += Main.EXCEPTED_SLEEP_TIME;
 				long sleepTime = (lastTick) - System.currentTimeMillis();
 				if (sleepTime < Main.CANT_KEEP_UP_TIMEOUT) {
-					this.warn("Can't keep up! " + (sleepTime / Main.EXCEPTED_SLEEP_TIME) + " ticks behind!");
+					this.warn("Can't keep up! " + -(sleepTime / Main.EXCEPTED_SLEEP_TIME) + " ticks behind!");
 					this.lastTick = System.currentTimeMillis();
 				} else if (sleepTime > Main.MAX_SLEEP) {
 					this.warn("Did the system time change?");
