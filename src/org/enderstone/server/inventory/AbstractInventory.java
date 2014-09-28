ce8e4f0195849940c9d5a1c7fe2118e75c8ad9b5
Fernando van Loenhout <ferrytinymailbox@gmail.com>
2014-09-28 19:06:32 +0200

    Restructured AbstractInventory.onItemClick, only right mouse button for normal slots is missing

--- src/org/enderstone/server/inventory/AbstractInventory.java
+++ src/org/enderstone/server/inventory/AbstractInventory.java
@@ -24,98 +24,117 @@ public abstract class AbstractInventory implements Inventory {
 
 	@Override
 	public void onItemClick(boolean leftMouse, int mode, int slot, boolean shiftClick, List<ItemStack> cursor) {
-		ItemStack existingStack = this.getRawItem(slot);
-		if (leftMouse) {
-			DropType slotDropType = this.getSlotDropType(slot);
-			if (shiftClick && slotDropType == DropType.FULL_OUT) {
-				if(existingStack == null)
-					return;
-				ItemStack lastResult = existingStack.clone();
-				do {
-					shiftClickFromMainInventory(slot);
-					existingStack = this.getRawItem(slot);
-				} while (!lastResult.equals(existingStack));
-			} else if (shiftClick) {
-				shiftClickFromMainInventory(slot);
+		ItemStack slotItemStack = this.getRawItem(slot);
+		DropType slotDropType = this.getSlotDropType(slot);
+		if (shiftClick && slotDropType == DropType.FULL_OUT) {
+			if (slotItemStack == null)
 				return;
-			}
-			assert !shiftClick;
-			switch (slotDropType) {
-				case FULL_OUT: {
-					if (existingStack == null) {
-						return; // Nothing happens if the slot is get-only & isempty
+			ItemStack lastResult = slotItemStack.clone();
+			// Keep crafting util all items on the crafting table that gives the desired recipe are crafted
+			do {
+				shiftClickFromMainInventory(slot, true);
+				slotItemStack = this.getRawItem(slot);
+			} while (!lastResult.equals(slotItemStack));
+		} else if (shiftClick) {
+			shiftClickFromMainInventory(slot, false);
+			return;
+		}
+		assert !shiftClick;
+		switch (slotDropType) {
+			case FULL_OUT:
+			case ONLY_OUT: {
+				if (slotItemStack == null) {
+					return; // Nothing happens if the slot is get-only & isempty
+				}
+				// DropType.FULL_OUT, the same happens with left and rigth clicks
+				int maxStackSize = slotItemStack.getId().getMaxStackSize();
+				ItemStack cursorItem = cursor.get(0);
+				if (cursorItem == null && slotDropType == DropType.ONLY_OUT) {
+					// Weird furnace behavior inside vanila minecraft
+					int amout = slotItemStack.getAmount();
+					int otherAmount = amout / 2;
+					int cursorAmount = amout - otherAmount;
+					cursor.set(0, cursorItem = slotItemStack.clone());
+					cursorItem.setAmount(cursorAmount);
+					if (otherAmount == 0)
+						this.getRawItems().set(slot, null);
+					else {
+						slotItemStack.setAmount(otherAmount);
+						this.getRawItems().set(slot, slotItemStack);
 					}
-					int maxStackSize = existingStack.getId().getMaxStackSize();
-					ItemStack cursorItem = cursor.get(0);
-					if (cursorItem == null) {
-						cursor.set(0, existingStack);
+				} else if (cursorItem == null) {
+					cursor.set(0, slotItemStack);
+					this.setRawItem(slot, null);
+				} else if (cursorItem.materialTypeMatches(slotItemStack)) {
+					int remainingItems = maxStackSize - cursorItem.getAmount();
+					if (remainingItems >= slotItemStack.getAmount()) {
+						cursorItem.setAmount(cursorItem.getAmount() + slotItemStack.getAmount());
 						this.setRawItem(slot, null);
-					} else if (cursorItem.materialTypeMatches(existingStack)) {
-						int remainingItems = maxStackSize - cursorItem.getAmount();
-						if (remainingItems >= existingStack.getAmount()) {
-							cursorItem.setAmount(cursorItem.getAmount() + existingStack.getAmount());
-							this.setRawItem(slot, null);
-							cursor.set(0, cursorItem);
-						}
+						cursor.set(0, cursorItem);
 					}
 				}
-				break;
-				case ARMOR_BOOTS_ONLY:
-				case ARMOR_CHESTPLATE_ONLY:
-				case ARMOR_HELMET_ONLY:
-				case ARMOR_LEGGINGS_ONLY: {
-					ItemStack cursorItem = cursor.get(0);
-					Armor armor = cursorItem == null ? null : Armor.fromId(cursor.get(0).getId());
-					boolean isCursorValidArmorItem = armor == null ? false : armor.getDropType() == slotDropType;
-					if (!isCursorValidArmorItem)
-						return;
-					ItemStack target = getRawItems().get(slot);
-					if (cursorItem == null || target == null || !target.materialTypeMatches(cursorItem)) {
-						swapItems(cursor, 0, getRawItems(), slot);
-					} else {
-						int cursorAmount = cursorItem.getAmount();
-						int newTargetAmount = Math.min(target.getAmount() + cursorAmount, target.getId().getMaxStackSize());
-						if (newTargetAmount != target.getAmount()) {
-							cursorAmount -= newTargetAmount - target.getAmount();
-							if (cursorAmount > 0) {
-								cursorItem.setAmount(cursorAmount);
-								cursor.set(0, cursorItem);
-							} else cursor.set(0, null);
-							target.setAmount(newTargetAmount);
-							getRawItems().set(slot, target);
-						}
-					}
-				}
-				break;
-				case ALL_ALLOWED: {
-					ItemStack cursorItem = cursor.get(0);
-					ItemStack target = getRawItems().get(slot);
-					if (cursorItem == null || target == null || !target.materialTypeMatches(cursorItem)) {
-						swapItems(cursor, 0, getRawItems(), slot);
-					} else {
-						int cursorAmount = cursorItem.getAmount();
-						int newTargetAmount = Math.min(target.getAmount() + cursorAmount, target.getId().getMaxStackSize());
-						if (newTargetAmount != target.getAmount()) {
-							cursorAmount -= newTargetAmount - target.getAmount();
-							if (cursorAmount > 0) {
-								cursorItem.setAmount(cursorAmount);
-								cursor.set(0, cursorItem);
-							} else cursor.set(0, null);
-							target.setAmount(newTargetAmount);
-							getRawItems().set(slot, target);
-						}
-					}
-				}
-				break;
 			}
+			break;
+			case ARMOR_BOOTS_ONLY:
+			case ARMOR_CHESTPLATE_ONLY:
+			case ARMOR_HELMET_ONLY:
+			case ARMOR_LEGGINGS_ONLY: {
+				// No need to add more code for rigth click, armor cannot stack and with a stack size of 1, 
+				//  Left and rigth mouse button presses work the same
+				ItemStack cursorItem = cursor.get(0);
+				Armor armor = cursorItem == null ? null : Armor.fromId(cursor.get(0).getId());
+				boolean isCursorValidArmorItem = armor == null ? false : armor.getDropType() == slotDropType;
+				if (!isCursorValidArmorItem)
+					return;
+				ItemStack target = getRawItems().get(slot);
+				if (cursorItem == null || target == null || !target.materialTypeMatches(cursorItem)) {
+					swapItems(cursor, 0, getRawItems(), slot);
+				} else {
+					int cursorAmount = cursorItem.getAmount();
+					int newTargetAmount = Math.min(target.getAmount() + cursorAmount, target.getId().getMaxStackSize());
+					if (newTargetAmount != target.getAmount()) {
+						cursorAmount -= newTargetAmount - target.getAmount();
+						if (cursorAmount > 0) {
+							cursorItem.setAmount(cursorAmount);
+							cursor.set(0, cursorItem);
+						} else cursor.set(0, null);
+						target.setAmount(newTargetAmount);
+						getRawItems().set(slot, target);
+					}
+				}
+			}
+			break;
+			default: {
+				if (leftMouse) {
+					ItemStack cursorItem = cursor.get(0);
+					ItemStack target = getRawItems().get(slot);
+					if (cursorItem == null || target == null || !target.materialTypeMatches(cursorItem)) {
+						swapItems(cursor, 0, getRawItems(), slot);
+					} else {
+						int cursorAmount = cursorItem.getAmount();
+						int newTargetAmount = Math.min(target.getAmount() + cursorAmount, target.getId().getMaxStackSize());
+						if (newTargetAmount != target.getAmount()) {
+							cursorAmount -= newTargetAmount - target.getAmount();
+							if (cursorAmount > 0) {
+								cursorItem.setAmount(cursorAmount);
+								cursor.set(0, cursorItem);
+							} else cursor.set(0, null);
+							target.setAmount(newTargetAmount);
+							getRawItems().set(slot, target);
+						}
+					}
+				} else {
 
-		} else {
-			assert leftMouse == false;
+				}
+
+			}
+			break;
 
 		}
+
 	}
 
-	private void shiftClickFromMainInventory(int slot) {
+	private void shiftClickFromMainInventory(int slot, boolean onlyWholeStackOut) {
 		ItemStack existingStack = this.getRawItem(slot);
 		int maxStackSize = existingStack.getId().getMaxStackSize();
 		int neededAmount = existingStack.getAmount();
@@ -134,7 +153,7 @@ public abstract class AbstractInventory implements Inventory {
 					neededAmount -= remainingItems;
 			}
 		}
-		if (neededAmount > 0)
+		if (neededAmount > 0 && onlyWholeStackOut)
 			return;
 		for (int i : this.getShiftClickLocations(slot)) {
 			ItemStack item = this.getRawItem(i);
