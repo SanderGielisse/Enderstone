c1f168ba86081f22af3b656618c7b48cd75d4b97
Fernando van Loenhout <ferrytinymailbox@gmail.com>
2014-10-01 21:37:34 +0200

    Fixed wrong item order when picking up items

--- src/org/enderstone/server/inventory/PlayerInventory.java
+++ src/org/enderstone/server/inventory/PlayerInventory.java
@@ -90,7 +90,7 @@ public class PlayerInventory extends DefaultInventory {
 
 	public List<ItemStack> getItemInsertionOrder() {
 		if (itemInsertionOrder != null) return itemInsertionOrder;
-		return itemInsertionOrder = new MergedList.Builder<ItemStack>().addList(0, getHotbar(), 0, 9).addList(9, getTopInventory(), 9, 3 * 9).build();
+		return itemInsertionOrder = new MergedList.Builder<ItemStack>().addList(0, getHotbar(), 0, 9).addList(9, getTopInventory(), 0, 3 * 9).build();
 	}
 
 	@Override
