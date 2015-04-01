b6bd69af4ddcaa6d318d425a2c695104b7d76ee2
Fernando van Loenhout <ferrytinymailbox@gmail.com>
2015-04-01 22:18:32 +0200

    Added javadoc comments to chunkmanager & added createchunk

--- src/org/enderstone/server/regions/io/ChunkManager.java
+++ src/org/enderstone/server/regions/io/ChunkManager.java
@@ -19,20 +19,47 @@
 package org.enderstone.server.regions.io;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
-import java.util.Set;
+import java.util.Map;
+import java.util.Random;
+import org.enderstone.server.regions.BlockData;
+import org.enderstone.server.regions.BlockId;
 import org.enderstone.server.regions.ChunkGenerator;
 import org.enderstone.server.regions.EnderChunk;
+import org.enderstone.server.regions.EnderWorld;
+import static org.enderstone.server.regions.EnderWorld.AMOUNT_OF_CHUNKSECTIONS;
+import org.enderstone.server.regions.RegionSet;
 
 public class ChunkManager {
 	
-	private ChunkGenerator generator;
+    /**
+     * Stored the chunk generator fo this world
+     */
+	private final ChunkGenerator generator;
 	
-	private File regionDirectory;
+    /**
+     * Region files of this world
+     */
+	private final File regionDirectory;
 	
-	private List<EnderChunk> unusedChunks;
+    /**
+     * Thrown away chunks that are unloaded, these objects are kept inside this
+     * list so we can reuse the memory without the garbage collector
+     * causing lagg
+     */
+	private final List<EnderChunk> unusedChunks = new ArrayList<>();
 	
-	private Set<EnderChunk> loadedChunks;
+    private final RegionSet loadedChunks;
+    private final EnderWorld world;
+    
+    public ChunkManager(ChunkGenerator generator, File regionDirectory, EnderWorld world) {
+        this.generator = generator;
+        this.regionDirectory = regionDirectory;
+        this.loadedChunks = new RegionSet();
+        this.world = world;
+    }
 	
 	public EnderChunk getChunk(int x, int z) {
 		return null;
@@ -44,5 +71,35 @@ public class ChunkManager {
     
     public void markChunkUsed(int chunkX, int chunkZ) {
         
+    }
+    
+    private EnderChunk createChunk(int x, int z) {
+        EnderChunk r;
+        BlockId[][] blocks = generator.generateExtBlockSections(world, new Random((((long)x) << 32) ^ z), x, z);
+		if (blocks == null) {
+			blocks = new BlockId[AMOUNT_OF_CHUNKSECTIONS][];
+		}
+		if (blocks.length != AMOUNT_OF_CHUNKSECTIONS) {
+			blocks = new BlockId[AMOUNT_OF_CHUNKSECTIONS][];
+		}
+		short[][] id = new short[16][];
+		byte[][] data = new byte[16][];
+		for (int i = 0; i < blocks.length; i++) {
+
+			if (blocks[i] != null) {
+				id[i] = new short[4096];
+				data[i] = new byte[4096];
+				for (int j = 0; j < 4096; j++) {
+
+					if (blocks[i][j] == null) {
+						id[i][j] = BlockId.AIR.getId();
+					} else {
+						id[i][j] = blocks[i][j].getId();
+					}
+				}
+			}
+		}
+		r = new EnderChunk(world, x, z, id, data, new byte[16 * 16], new ArrayList<BlockData>());
+		return r;
     }
 }
